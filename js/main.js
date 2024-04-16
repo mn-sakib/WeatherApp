d9797f245d09a957b380882a2ceefd99f03466d5
Md Najmus Sakib <mnsakib.me@gmail.com>
2024-04-17 00:11:31 +0600

    Fixed some styles and update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-
-const apiKey = "adfcaba9752caeefe73fb6f8dc85988e"
+/*  Warning!!!
+Do not use this apiKey! for generating your own,
+please open a account with www.openweathermap.org | it's 100% free to use! */
+const apiKey = "adfcaba9752caeefe73fb6f8dc85988e" // Do not use this apiKey!
 const apiURL = `https://api.openweathermap.org/data/2.5/weather?units=metric`
 let searchBtn = document.querySelector('.search-btn');
 let bgImg = document.querySelector('.wrapper');
@@ -10,6 +12,7 @@ async function checkWeather(city){
     if (response.status == 404){
         document.querySelector('.temperature-box').style.display = 'none'
         document.querySelector('.error').style.display = 'block'
+        document.querySelector('.location').style.display = 'none'
     } else{
         let data = await response.json();
 
@@ -17,6 +20,7 @@ async function checkWeather(city){
         document.querySelector('.temp p').innerHTML = data.weather[0].description;
         document.querySelector('.humidity h3').innerHTML = data.main.humidity + '%';
         document.querySelector('.wind h3').innerHTML = Math.round(data.wind.speed) + ' km/h';
+        document.querySelector('.location h3').textContent = "Location: " + data.name;
     
         if (data.weather[0].main == "Clouds"){
             tempIcon.src = "./img/cloud.png";
@@ -31,15 +35,15 @@ async function checkWeather(city){
         }
     
         document.querySelector('.temperature-box').style.display = 'block'
+        document.querySelector('.location').style.display = 'block'
         document.querySelector('.error').style.display = 'none'
+
     }
-    
 }
 searchBtn.addEventListener('click', () => {
     let searchValue = document.querySelector(".searchBox").value;
     checkWeather(searchValue);
     document.querySelector(".searchBox").value = "";
-    
 })
 
 
